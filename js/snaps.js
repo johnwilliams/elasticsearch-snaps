c9880f4466a64a67b573d9843bc69d405cdd953f
John Williams <john@veloshots.com>
2016-02-18 15:13:52 -0600

    add number of shards

--- js/snaps.js
+++ js/snaps.js
@@ -7,14 +7,14 @@ $.getJSON(es_endpoint + "_snapshot", function(snapshot_config) {
   return (getNextSnapshot = function() {
     var key;
     if (key = keys.shift()) {
-      html += "<li>" + key + "<table><th>Name</th><th>Date</th><th>State</th><th>Operations</th>";
+      html += "<li>" + key + "<table><th>name</th><th>date</th><th>state</th><th>shards</th><th>operations</th>";
       return $.getJSON(es_endpoint + "_snapshot/" + key + "/_all", function(snapshots) {
         var i, len, ref, value;
         ref = snapshots.snapshots;
         for (i = 0, len = ref.length; i < len; i++) {
           value = ref[i];
           var snapshot_url = es_endpoint + "_snapshot/" + key + "/" + value.snapshot + "/_restore"
-          html += "<tr><td>" + value.snapshot + "</td><td>" + value.start_time + "</td><td>" + value.state + "</td><td>" + "[<a href='javascript:Restore(\"" + key + "\",\"" + value.snapshot + "\")'>restore</a>|<a href='javascript:Delete(\"" + key + "\",\"" + value.snapshot + "\")'>delete</a>]" + "</td></tr>";
+          html += "<tr><td>" + value.snapshot + "</td><td>" + value.start_time + "</td><td>" + value.state + "</td><td>" + value.shards.total + "</td><td>" + "[<a href='javascript:Restore(\"" + key + "\",\"" + value.snapshot + "\")'>restore</a>|<a href='javascript:Delete(\"" + key + "\",\"" + value.snapshot + "\")'>delete</a>]" + "</td></tr>";
         }
         html += "</table></li>";
         return getNextSnapshot();
